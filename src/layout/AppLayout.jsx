e0e3b580bb146387e2da01d0ae36bffc1ec2201b
김예린 <yerin.3321@daum.net>
2024-09-01 01:07:24 +0900

    link update2

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -6,7 +6,7 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Outlet } from "react-router-dom";
-import { LinkContainer } from "react-router-bootstrap";
+import LinkContainer from "react-router-bootstrap";
 
 const AppLayout = () => {
   return (
