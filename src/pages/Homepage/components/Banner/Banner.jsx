d71041dc72fdbe155578f304c3ef73b658a6e6d2
김예린 <yerin.3321@daum.net>
2024-09-01 20:34:41 +0900

    [2024.09.01] banner

---
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import "./Banner.style.css";
+import { usePopularMoviesQuery } from "../../../../hooks/userPopularMovies";
+import Alert from "react-bootstrap/Alert";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const Banner = () => {
+  const { data, isLoading, isError, error } = usePopularMoviesQuery();
+  console.log("ddd", data);
+  if (isLoading) {
+    <Alert>Loading...</Alert>;
+  } else if (isError) {
+    <Alert variant="danger">{error.message}</Alert>;
+  }
+  return (
+    <div className="banner_container">
+      <Swiper
+        slidesPerView={1}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide
+          style={{
+            backgroundImage: `url(
+          https://media.themoviedb.org/t/p/w533_and_h300_bestv2/${data?.results[0].backdrop_path}
+          )`,
+          }}
+        >
+          <div className="inner_container">
+            <div className="txtbox text-white">
+              <h1>{data?.results[0].title}</h1>
+              <p>{data?.results[0].overview}</p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide
+          style={{
+            backgroundImage: `url(
+          https://media.themoviedb.org/t/p/w533_and_h300_bestv2/${data?.results[1].backdrop_path}
+          )`,
+          }}
+        >
+          <div className="inner_container">
+            <div className="txtbox text-white">
+              <h1>{data?.results[1].title}</h1>
+              <p>{data?.results[1].overview}</p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide
+          style={{
+            backgroundImage: `url(
+          https://media.themoviedb.org/t/p/w533_and_h300_bestv2/${data?.results[2].backdrop_path}
+          )`,
+          }}
+        >
+          <div className="inner_container">
+            <div className="txtbox text-white">
+              <h1>{data?.results[2].title}</h1>
+              <p>{data?.results[2].overview}</p>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Banner;
